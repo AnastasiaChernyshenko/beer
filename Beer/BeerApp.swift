24341a43131574ec7187eae023588bae5812f4ce
AnastasiaRailian <a.railian@do-it.co>
2022-10-18 09:27:04 +0300

    Initial Commit

---
+++ Beer/BeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerApp.swift
+//  Beer
+//
+//  Created by Anastasia on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beer
+//
+//  Created by Anastasia on 18.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
